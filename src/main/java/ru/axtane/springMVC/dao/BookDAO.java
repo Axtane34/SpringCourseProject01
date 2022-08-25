5ec8cb37b268852e6cdddb63b404ea3f73db54e7
Axtane <axtane3456@gmail.com>
2022-08-25 05:56:11 +0300

    fix misunderstanding session creation from jpa wrapper in comments

--- src/main/java/ru/axtane/springMVC/dao/BookDAO.java
+++ src/main/java/ru/axtane/springMVC/dao/BookDAO.java
@@ -2,68 +2,64 @@ package ru.axtane.springMVC.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
 
 @Component
 public class BookDAO {
-    private final EntityManagerFactory entityManagerFactory;
+    private final EntityManager entityManager;
 
     @Autowired
-    public BookDAO(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
+    public BookDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
     
     /*@Transactional(readOnly = true)
     public List<Book> index(){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        return entityManager.createQuery("select b from Book b", Book.class).getResultList();
+        Session session = entityManager.unwrap(Session.class);
+        return session.createQuery("select b from Book b", Book.class).getResultList();
         *//*return jdbcTemplate.query("SELECT * FROM book", new BeanPropertyRowMapper<>(Book.class));*//*
     }
 
     @Transactional(readOnly = true)
     public Book show(int id){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        return entityManager.find(Book.class, id);
+        Session session = entityManager.unwrap(Session.class);
+        return session.get(Book.class, id);
     }
 
     @Transactional
     public void save(Book book){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.persist(book);
+        Session session = entityManager.unwrap(Session.class);
+        session.persist(book);
         *//*jdbcTemplate.update("INSERT INTO book(name, author, yearOfWriting) VALUES(?, ?, ?)", book.getName(), book.getAuthor(), book.getYearOfWriting());*//*
     }
     @Transactional
     public void update(int id, Book updatedBook){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Book book = entityManager.find(Book.class, id);
+        Session session = entityManager.unwrap(Session.class);
+        Book book = session.get(Book.class, id);
         Book.updateBook(book, updatedBook);
 
         *//*jdbcTemplate.update("UPDATE book SET name=?, author=?, yearOfWriting=? WHERE book_id=?", book.getName(), book.getAuthor(), book.getYearOfWriting(), id);*//*
     }*/
     /*@Transactional
     public void updateOwner(int id, int person_id){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Book book = entityManager.find(Book.class, id);
-        Person person = entityManager.find(Person.class, person_id);
+        Session session = entityManager.unwrap(Session.class);
+        Book book = session.get(Book.class, id);
+        Person person = session.get(Person.class, person_id);
         person.addBook(book);
-        entityManager.persist(person);
-        entityManager.persist(book);
         *//*jdbcTemplate.update("UPDATE book SET person_id=? WHERE book_id=?", person_id, id);*//*
     }
     @Transactional
     public void deleteOwner(int id){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Book book = entityManager.find(Book.class, id);
+        Session session = entityManager.unwrap(Session.class);
+        Book book = session.get(Book.class, id);
         book.getOwner().getBooks().remove(book);
         book.setOwner(null);
-        entityManager.persist(book.getOwner());
-        entityManager.persist(book);
         *//*jdbcTemplate.update("UPDATE book SET person_id=NULL WHERE book_id=?", id);*//*
     }*/
     /*@Transactional
     public void delete(int id){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.remove(entityManager.find(Book.class, id));
+        Session session = entityManager.unwrap(Session.class);
+        session.remove(session.get(Book.class, id));
         *//*jdbcTemplate.update("DELETE FROM book WHERE book_id=?", id);*//*
     }*/
 }
--- src/main/java/ru/axtane/springMVC/dao/PersonDAO.java
+++ src/main/java/ru/axtane/springMVC/dao/PersonDAO.java
@@ -6,57 +6,58 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import ru.axtane.springMVC.models.Person;
-import javax.persistence.EntityManagerFactory;
+
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 @Component
 public class PersonDAO {
     private final JdbcTemplate jdbcTemplate;
-    private final EntityManagerFactory entityManagerFactory;
+    private final EntityManager entityManager;
 
     @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate, EntityManagerFactory entityManagerFactory) {
+    public PersonDAO(JdbcTemplate jdbcTemplate, EntityManager entityManager) {
         this.jdbcTemplate = jdbcTemplate;
-        this.entityManagerFactory = entityManagerFactory;
+        this.entityManager = entityManager;
     }
     /*@Transactional(readOnly = true)
     public List<Person> index(){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        return entityManager.createQuery("select p from Person p", Person.class).getResultList();
+        Session session = entityManager.unwrap(Session.class);
+        return session.createQuery("select p from Person p", Person.class).getResultList();
         *//*return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));*//*
     }
     @Transactional(readOnly = true)
     public Person show(int id){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        return entityManager.find(Person.class, id);
+        Session session = entityManager.unwrap(Session.class);
+        return session.get(Person.class, id);
         *//*return jdbcTemplate.query("SELECT * FROM person WHERE person_id=?",
                 new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);*//*
     }*/
     /*@Transactional(readOnly = true)
     public Optional<Person> show(String fio){
-        *//*EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Person person = entityManager.createQuery("from Person where fio=:fio", Person.class).setParameter("fio", fio).getSingleResult();
+        *//*Session session = entityManager.unwrap(Session.class);
+        Person person = session.createQuery("from Person where fio=:fio", Person.class).setParameter("fio", fio).getSingleResult();
         return person;*//*
         return jdbcTemplate.query("SELECT * FROM person WHERE fio=?",
                 new Object[]{fio}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }*/
     /*@Transactional
     public void save(Person person){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.persist(person);
+        Session session = entityManager.unwrap(Session.class);
+        session.persist(person);
         *//*jdbcTemplate.update("INSERT INTO person(fio, yearOfBirth) VALUES(?, ?)", person.getFio(), person.getYearOfBirth());*//*
     }
     @Transactional
     public void update(int id, Person updatedPerson){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Person person = entityManager.find(Person.class, id);
+        Session session = entityManager.unwrap(Session.class);
+        Person person = session.get(Person.class, id);
         Person.updatePerson(person, updatedPerson);
         *//*jdbcTemplate.update("UPDATE person SET fio=?, yearOfBirth=? WHERE person_id=?", person.getFio(), person.getYearOfBirth(), id);*//*
     }
     @Transactional
     public void delete(int id){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.remove(entityManager.find(Person.class, id));
+        Session session = entityManager.unwrap(Session.class);
+        session.remove(session.get(Person.class, id));
         *//*jdbcTemplate.update("DELETE FROM person WHERE person_id=?", id);*//*
     }*/
 }
